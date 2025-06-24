b1ede0a330ee530074324a5f66ec47d409b9072b
ellora11 <159638183+ellora11@users.noreply.github.com>
2025-06-24 18:39:22 +0530

    Assignment 2

---
+++ Assignment_2_task_1.py
@@ -0,0 +1,7 @@
+i = int(input("Enter a number: "))
+if i % 2 == 0:
+    print(f"{i} is an even number.")
+elif i % 2 == 1:
+    print(f"{i} is an odd number.")
+else:
+    print("None")
---
+++ Assignment_2_task_2.py
@@ -0,0 +1,5 @@
+num = range(1,51)
+sum = 0
+for i in num:
+    sum += i
+print(f"The sum of numbers from 1 to 50 is: {sum}")
